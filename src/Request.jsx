022d0140345dfbe5e789ba3dcaa4fd971c2e064e
datnguyen <dat708710@gmail.com>
2023-08-03 00:05:34 +0700

    commit

--- src/Request.jsx
+++ src/Request.jsx
@@ -1,6 +1,6 @@
 const Request = {
-    // baseUrl: 'https://worktop.somee.com',
-    baseUrl: 'https://localhost:44393',
+    baseUrl: 'https://worktop.somee.com',
+    // baseUrl: 'https://localhost:44393',
     //baseUrl: 'https://192.168.2.23:44393',
     apiKey:'54c7a2ef8f64c2be42e7ee535dfc76bb'
 }
